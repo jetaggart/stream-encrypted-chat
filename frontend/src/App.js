fa88334d5e13470ef3472c883832bf68b58d7e91
Jeff Taggart <jeff@jetaggart.com>
2019-09-21 16:53:09 -0600

    Split channel/public key from registration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { Channel, ChannelHeader, Chat, MessageList, Thread, Window } from 'stream-chat-react';
-import { StreamChat } from 'stream-chat';
 import { MessageEncrypted } from './MessageEncrypted';
-import { EThree } from '@virgilsecurity/e3kit';
-import { post } from './Http'
 
 import 'stream-chat-react/dist/css/index.css';
 import { MessageInputEncrypted } from "./MessageInputEncrypted";
@@ -12,70 +9,9 @@ import { StartChat } from "./StartChat";
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { identity: '', chatWith: '', error: '' };
+    this.state = {};
   }
 
-  _connectStream = (response) => {
-    const chatClient = new StreamChat(response.apiKey);
-
-    chatClient.setUser(
-      {
-        id:    response.user.id,
-        name:  response.user.name,
-        image: response.user.image
-      },
-      response.token
-    );
-
-    let members = [this.state.identity, this.state.chatWith];
-    members.sort();
-
-    const channel = chatClient.channel('messaging', {
-      image:   `https://getstream.io/random_svg/?id=rapid-recipe-0&name=${members.join("+")}`,
-      name:    members.join(", "),
-      members: members,
-    });
-
-    this.setState({
-      stream: {
-        ...response,
-        client:  chatClient,
-        channel: channel
-      }
-    });
-  };
-
-  _connectVirgil = async (response) => {
-    const eThree = await EThree.initialize(() => response.token);
-    try {
-      await eThree.register();
-    } catch {
-      // already registered;
-    }
-
-    try {
-      const publicKeys = await eThree.lookupPublicKeys([this.state.identity, this.state.chatWith]);
-      this.setState({
-        virgil: {
-          ...response,
-          eThree,
-          publicKeys
-        }
-      });
-    } catch {
-      this.setState({
-        error: 'Other user is not registered. Open another window and register that identity. Refresh browser to start over.'
-      })
-    }
-  };
-
-  _connect = (authToken) => {
-    post("http://localhost:8080/v1/stream-token", {}, authToken)
-      .then(this._connectStream);
-
-    post("http://localhost:8080/v1/virgil-token", {}, authToken)
-      .then(this._connectVirgil);
-  };
 
   _buildMessageEncrypted = (props) => {
     const newProps = {
@@ -87,25 +23,11 @@ class App extends React.Component {
     return <MessageEncrypted {...newProps}/>
   };
 
-  setChat = (data) => {
-    this.setState({
-      identity: data.identity,
-      chatWith: data.chatWith,
-    });
-    this._connect(data.authToken);
+  onConnect = (chatData) => {
+    this.setState(chatData);
   };
 
   render = () => {
-    if (this.state.error !== '') {
-      return (
-        <div className="container">
-          <div className="card">
-            {this.state.error}
-          </div>
-        </div>
-      )
-    }
-
     if (this.state.stream && this.state.virgil) {
       return (
         <Chat client={this.state.stream.client} theme={'messaging light'}>
@@ -120,7 +42,7 @@ class App extends React.Component {
         </Chat>
       )
     } else {
-      return <StartChat setChat={this.setChat}/>
+      return <StartChat onConnect={this.onConnect}/>
     }
   }
 }
--- frontend/src/StartChat.js
+++ frontend/src/StartChat.js
@@ -1,35 +1,107 @@
 import React, { PureComponent } from 'react';
-import { post } from "./Http";
+import { StreamChat } from 'stream-chat';
+import { EThree } from '@virgilsecurity/e3kit';
+import { post } from './Http'
 
 export class StartChat extends PureComponent {
   constructor(props) {
     super(props);
-    this.state = { identity: '', chatWith: '', step: 'identity' };
-  }
+    this.state = { identity: '', chatWith: '', error: '' }
+  };
 
   _handleIdentityChange = (event) => {
     this.setState({ identity: event.target.value });
-  };
-
-  _handleNext = (event) => {
-    event.preventDefault();
-    this.setState({
-      step: 'chatWith'
-    })
   };
 
   _handleChatWithChange = (event) => {
     this.setState({ chatWith: event.target.value });
   };
 
-  _handleSubmit = (event) => {
+  _handleNext = async (event) => {
     event.preventDefault();
     post("http://localhost:8080/v1/authenticate", { identity: this.state.identity })
-      .then(res => this.props.setChat({
-        identity:  this.state.identity,
-        chatWith:  this.state.chatWith,
-        authToken: res.authToken
-      }));
+      .then(res => res.authToken)
+      .then(this._connect);
+  };
+
+  _handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const publicKeys = await this.state.virgil.eThree.lookupPublicKeys([this.state.identity, this.state.chatWith]);
+
+      let members = [this.state.identity, this.state.chatWith];
+      members.sort();
+
+      const channel = this.state.stream.client.channel('messaging', {
+        image:   `https://getstream.io/random_svg/?id=rapid-recipe-0&name=${members.join("+")}`,
+        name:    members.join(", "),
+        members: members,
+      });
+
+      this.props.onConnect({
+        virgil: {
+          ...this.state.virgil,
+          publicKeys
+        },
+        stream: {
+          ...this.state.stream,
+          channel
+        }
+      })
+    } catch (err) {
+      this.setState({
+        error: err.message
+      })
+    }
+  };
+
+  _connectStream = (response) => {
+    const client = new StreamChat(response.apiKey);
+
+    client.setUser(
+      {
+        id:    response.user.id,
+        name:  response.user.name,
+        image: response.user.image
+      },
+      response.token
+    );
+
+    return {
+      ...response,
+      client
+    };
+  };
+
+  _connectVirgil = async (response) => {
+    const eThree = await EThree.initialize(() => response.token);
+    try {
+      await eThree.register();
+    } catch {
+      // already registered;
+    }
+
+    try {
+      return {
+        ...response,
+        eThree,
+      };
+    } catch {
+      return {
+        error: 'Other user is not registered. Open another window and register that identity. Refresh browser to start over.'
+      };
+    }
+  };
+
+  _connect = async (authToken) => {
+    const stream = await post("http://localhost:8080/v1/stream-token", {}, authToken)
+      .then(this._connectStream);
+
+    const virgil = await post("http://localhost:8080/v1/virgil-token", {}, authToken)
+      .then(this._connectVirgil);
+
+    this.setState({ stream, virgil })
   };
 
   _buildForm = (field, title, subtitle, submitLabel, submit, handleFieldChange) => {
@@ -46,11 +118,19 @@ export class StartChat extends PureComponent {
   };
 
   render() {
-    if (this.state.step === 'identity') {
-      return this._buildForm('identity', 'Who are you?', '', 'Register', this._handleNext, this._handleIdentityChange);
-    } else {
+    if (this.state.error !== '') {
+      return (
+        <div className="container">
+          <div className="card">
+            {this.state.error}
+          </div>
+        </div>
+      )
+    } else if (this.state.virgil && this.state.stream) {
       const subtitle = 'Registered as "' + this.state.identity + '". Open another window to register another identity.';
       return this._buildForm('chatWith', 'Chat with?', subtitle, 'Start Chat', this._handleSubmit, this._handleChatWithChange);
+    } else {
+      return this._buildForm('identity', 'Who are you?', '', 'Register', this._handleNext, this._handleIdentityChange);
     }
   }
 }
