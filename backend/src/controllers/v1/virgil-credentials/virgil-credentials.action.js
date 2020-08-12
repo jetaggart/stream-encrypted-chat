e6b35287bca16b5063297df28c1cdf7ce6299f97
cardoso <matheus@cardo.so>
2020-08-12 10:50:51 -0300

    Update Virgil libs

--- backend/src/controllers/v1/virgil-credentials/virgil-credentials.action.js
+++ backend/src/controllers/v1/virgil-credentials/virgil-credentials.action.js
@@ -1,22 +1,36 @@
 import dotenv from 'dotenv';
 
 const { JwtGenerator } = require('virgil-sdk');
-const { VirgilCrypto, VirgilAccessTokenSigner } = require('virgil-crypto');
+const { initCrypto, VirgilCrypto, VirgilAccessTokenSigner } = require('virgil-crypto');
 
 dotenv.config();
 
-const virgilCrypto = new VirgilCrypto();
+async function getJwtGenerator() {
+	await initCrypto();
 
-const generator = new JwtGenerator({
-  appId: process.env.VIRGIL_APP_ID,
-  apiKeyId: process.env.VIRGIL_KEY_ID,
-  apiKey: virgilCrypto.importPrivateKey(process.env.VIRGIL_PRIVATE_KEY),
-  accessTokenSigner: new VirgilAccessTokenSigner(virgilCrypto)
-});
+	const virgilCrypto = new VirgilCrypto();
+	// initialize JWT generator with your App ID and App Key ID you got in
+	// Virgil Dashboard.
+	return new JwtGenerator({
+		appId: process.env.VIRGIL_APP_ID,
+		apiKeyId: process.env.VIRGIL_KEY_ID,
+		// import your App Key that you got in Virgil Dashboard from string.
+		apiKey: virgilCrypto.importPrivateKey(process.env.VIRGIL_PRIVATE_KEY),
+		// initialize accessTokenSigner that signs users JWTs
+		accessTokenSigner: new VirgilAccessTokenSigner(virgilCrypto),
+		// JWT lifetime - 20 minutes (default)
+		millisecondsToLive:  20 * 60 * 1000
+	});
+}
 
+const generatorPromise = getJwtGenerator();
 
 exports.virgilCredentials = async (req, res) => {
+  const generator = await generatorPromise;
+  // Get the identity of the user making the request (this assumes the request
+  // is authenticated and there is middleware in place that populates the
+  // `req.user` property with the user record).
   const virgilJwtToken = generator.generateToken(req.user.sender);
-
+  // Send it to the authorized user
   res.json({ token: virgilJwtToken.toString() });
 };
